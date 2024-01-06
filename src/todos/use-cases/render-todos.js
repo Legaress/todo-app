15716e0e38468e309927bce3369a5afe296a69b8
Lesnier Garcia Estrada <lesier.estrada@gmail.com>
2024-01-06 14:19:20 -0500

    Implementar mejoras en render-todos.js

--- src/todos/use-cases/render-todos.js
+++ src/todos/use-cases/render-todos.js
@@ -1,19 +1,29 @@
 import { Todo } from '../models/todo.model';
-import { createTodoHTML } from './';
+import { createTodoHTML } from './'; 
 
-let element;
+let elementReference;
+
 /**
- * @param {String} elementId
- * @param {Todo} todos
+ * Renderiza una lista de tareas en el elemento especificado.
+ * @param {string} elementId - El ID del elemento HTML donde se van a renderizar las tareas.
+ * @param {Todo[]} todos - Lista de tareas a renderizar.
+ * @throws {Error} - Si el elemento no se encuentra en el DOM.
  */
 export const renderTodos = (elementId, todos = []) => {
-    //TODO: Refrencia
-    if(!element) element = document.querySelector(elementId);
-    if(!elementId) throw new Error(`Element ${elementId} not found`);
+  if (!elementReference) {
+    elementReference = document.querySelector(elementId);
+  }
 
-    element.innerHTML = '';
+  if (!elementReference) {
+    throw new Error(`Element with ID "${elementId}" not found`);
+  }
 
-    todos.forEach(todo => {
-        element.append( createTodoHTML(todo));
-    });
+  // Limpiar el contenido anterior en el elemento
+  elementReference.innerHTML = '';
+
+  // Renderizar cada tarea en el elemento
+  todos.forEach((todo) => {
+    const todoElement = createTodoHTML(todo);
+    elementReference.appendChild(todoElement);
+  });
 };
