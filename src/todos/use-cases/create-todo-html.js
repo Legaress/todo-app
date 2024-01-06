5ec00b50380a66cdf0b533fb1bd3342ac7bd318c
Lesnier Garcia Estrada <lesier.estrada@gmail.com>
2024-01-06 14:13:10 -0500

    Implementar mejoras en create-todo-html.js

--- src/todos/use-cases/create-todo-html.js
+++ src/todos/use-cases/create-todo-html.js
@@ -1,27 +1,32 @@
 import { Todo } from '../models/todo.model';
 
 /**
- * @param {Todo} todo
- * @returns {HTMLElement}
+ * Crea un elemento HTML representando una tarea.
+ * @param {Todo} todo - La tarea a representar.
+ * @returns {HTMLElement} - Elemento HTML que representa la tarea.
+ * @throws {Error} - Si no se proporciona un objeto tarea.
  */
 export const createTodoHTML = (todo) => {
-  if (!todo) throw new Error('A TODO object is required');
+  if (!todo) {
+    throw new Error('A TODO object is required');
+  }
 
-  const {id, description, done} = todo;
+  const { id, description, done } = todo;
+  const isChecked = done ? 'checked' : '';
+  const todoHTML = 
+    `<div class="view">
+      <input class="toggle" type="checkbox" ${isChecked}>
+      <label>${description}</label>
+      <button class="destroy"></button>
+    </div>
+    <input class="edit" value="Create a TodoMVC template">`;
 
-  const html = 
-              `<div class="view">
-                <input class="toggle" type="checkbox" ${ done ? 'checked' : '' }>
-                <label> ${description} </label>
-                <button class="destroy"></button>
-              </div>
-              <input class="edit" value="Create a TodoMVC template">`;
+  const todoListItem = document.createElement('li');
+  todoListItem.innerHTML = todoHTML;
+  todoListItem.setAttribute('data-id', id);
+  if (done) {
+    todoListItem.classList.add('completed');
+  }
 
-  const liElement = document.createElement('li');
-  liElement.innerHTML = html;
-  
-  liElement.setAttribute('data-id',id);
-  if(done) liElement.setAttribute('class','completed');
-
-  return liElement;
+  return todoListItem;
 };
